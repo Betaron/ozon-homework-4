9277b88952963a3cad8ff008adb18c0746a0f90c
Иван Агабалаев <agabalaev01@yandex.ru>
2023-04-01 16:49:41 +0300

    Make async writing

--- ParallelMaster/ParallelTransformer.cs
+++ ParallelMaster/ParallelTransformer.cs
@@ -9,14 +9,14 @@ public class ParallelTransformer<TInput, TOutput> : IDisposable
 {
     private uint _parallelismDegree;
 
-    private readonly object _inputLock = new();
-    private readonly object _outputLock = new();
-
     private Stack<CancellationTokenSource> _threadCts = new();
     private CancellationTokenSource _readInputCts = new();
-    private CancellationTokenSource _takeInputCts = new();
-    private List<Task> _tasks = new();
+    private CancellationTokenSource _writeOutputCts = new();
+    private List<Task> _calculationTasks = new();
+    private Task _readInputTask;
+    private Task _writeOutputTask;
     private BlockingCollection<TInput> _inputBuffer;
+    private BlockingCollection<TOutput> _outputBuffer;
     private CsvFileWorker _fileWorker;
 
     public Func<TInput, TOutput> TransformFunction { get; init; }
@@ -51,16 +51,17 @@ public class ParallelTransformer<TInput, TOutput> : IDisposable
         TransformFunction = transformFunction;
         InputPath = inputPath;
         OutputPath = outputPath ?? inputPath.Insert(inputPath.LastIndexOf('.'), "_calculated");
+        InputBufferSize = inputBufferSize < 1 ? 1 : inputBufferSize;
         ParallelismDegree = parallelismDegree;
-        InputBufferSize = inputBufferSize;
 
         _inputBuffer = new BlockingCollection<TInput>(inputBufferSize);
+        _outputBuffer = new BlockingCollection<TOutput>();
         _fileWorker = new(InputPath, OutputPath);
     }
 
     private void OnParallelismDegreeChanged()
     {
-        var difference = ParallelismDegree - _tasks.Count;
+        var difference = ParallelismDegree - _calculationTasks.Count;
 
         if (difference > 0)
         {
@@ -89,21 +90,19 @@ public class ParallelTransformer<TInput, TOutput> : IDisposable
 
         IsExecuting = true;
 
-        ReadInLimitBufferAsync(_fileWorker.CsvReader, _readInputCts.Token);
-
-        _fileWorker.CsvWriter.UseSnakeCaseHeaders<TOutput>();
-
-        _fileWorker.CsvWriter.WriteHeader<TOutput>();
-        _fileWorker.CsvWriter.NextRecord();
-        _fileWorker.CsvWriter.Flush();
+        _readInputTask = ReadInBufferAsync(_fileWorker.CsvReader, _readInputCts.Token);
+        _writeOutputTask = WriteFromBufferAsync(_fileWorker.CsvWriter, _writeOutputCts.Token);
 
         for (int i = 0; i < ParallelismDegree; i++)
         {
             StartNewThread();
         }
 
-        await Task.WhenAll(_tasks);
-        _tasks.Clear();
+        await Task.WhenAll(_calculationTasks);
+        _outputBuffer.CompleteAdding();
+        await _writeOutputTask;
+
+        _calculationTasks.Clear();
         _threadCts.Clear();
         IsExecuting = false;
     }
@@ -112,45 +111,28 @@ public class ParallelTransformer<TInput, TOutput> : IDisposable
     {
         var tokenSource = new CancellationTokenSource();
         _threadCts.Push(tokenSource);
-        _tasks.Add(TransformAsync(
-            _fileWorker.CsvReader,
-            _fileWorker.CsvWriter,
-            tokenSource.Token));
+        _calculationTasks.Add(TransformAsync(tokenSource.Token));
     }
 
     private Task TransformAsync(
-        CsvReader csvReader,
-        CsvWriter csvWriter,
         CancellationToken token)
     {
         return Task.Factory.StartNew(() =>
         {
-            while (!token.IsCancellationRequested)
+            while (!_inputBuffer.IsCompleted && !token.IsCancellationRequested)
             {
                 TInput inputRecord;
-
-                try
-                {
-                    inputRecord = _inputBuffer.Take(_takeInputCts.Token);
-                }
-                catch (OperationCanceledException)
-                {
-                    break;
-                }
+                try { inputRecord = _inputBuffer.Take(); }
+                catch (InvalidOperationException) { break; }
 
                 var result = TransformFunction.Invoke(inputRecord);
 
-                lock (_outputLock)
-                {
-                    csvWriter.WriteRecord(result);
-                    csvWriter.NextRecord();
-                    csvWriter.Flush();
-                }
+                _outputBuffer.Add(result);
             }
         }, token);
     }
 
-    private Task ReadInLimitBufferAsync(CsvReader csvReader, CancellationToken token)
+    private Task ReadInBufferAsync(CsvReader csvReader, CancellationToken token)
     {
         return Task.Factory.StartNew(() =>
         {
@@ -159,21 +141,61 @@ public class ParallelTransformer<TInput, TOutput> : IDisposable
 
             while (_fileWorker.CsvReader.Read() && !token.IsCancellationRequested)
             {
-                _inputBuffer.Add(csvReader.GetRecord<TInput>());
+                var record = csvReader.GetRecord<TInput>();
+                if (record == null)
+                {
+                    continue;
+                }
+
+                _inputBuffer.Add(record);
             }
-            _takeInputCts.Cancel();
+            _inputBuffer.CompleteAdding();
         }, token);
     }
 
-    public void Dispose()
+    private Task WriteFromBufferAsync(CsvWriter csvWriter, CancellationToken token)
     {
+        return Task.Factory.StartNew(() =>
+        {
+            csvWriter.UseSnakeCaseHeaders<TOutput>();
+
+            csvWriter.WriteHeader<TOutput>();
+            csvWriter.NextRecord();
+            csvWriter.Flush();
+
+            while (!_outputBuffer.IsCompleted && !token.IsCancellationRequested)
+            {
+                TOutput outputRecord;
+                try
+                {
+                    outputRecord = _outputBuffer.Take();
+                }
+                catch (OperationCanceledException)
+                {
+                    break;
+                }
+
+                csvWriter.WriteRecord(outputRecord);
+                csvWriter.NextRecord();
+                csvWriter.Flush();
+            }
+        }, token);
+    }
+
+    public async void Dispose()
+    {
+        _readInputCts.Cancel();
         foreach (var item in _threadCts)
         {
             item.Cancel();
         }
-        _readInputCts.Cancel();
-        Task.WaitAll(_tasks.ToArray());
+        _writeOutputCts.Cancel();
+        await _readInputTask;
+        await Task.WhenAll(_calculationTasks);
+        await _writeOutputTask;
         _inputBuffer.Dispose();
         _fileWorker.Dispose();
+
+        GC.SuppressFinalize(this);
     }
 }
